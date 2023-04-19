fa120b8502a8859b8f5fba9aa818722e9ad3eecf
felix9909 <long99090704@gmail.com>
2023-04-19 13:45:44 +0800

    初始化 Spring boot project

---
+++ src/main/java/com/felix/springbootbulletinboard/SpringbootBulletinboardApplication.java
@@ -0,0 +1,13 @@
+package com.felix.springbootbulletinboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBulletinboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBulletinboardApplication.class, args);
+    }
+
+}
